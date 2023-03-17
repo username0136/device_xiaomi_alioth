e8209415f7fb5beebec8f26d24c9190a02ce5377
JayanthKandula <k.jayanth94@gmail.com>
2023-03-18 00:04:54 +0530

    alioth: Include ximi firmware

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,3 +32,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
 include vendor/xiaomi/alioth/BoardConfigVendor.mk
+
+# Firmware
+include vendor/xiaomi/alioth-firmware/BoardConfigVendor.mk
